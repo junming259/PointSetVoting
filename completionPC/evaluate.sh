dbc18e3f7d35f53af19ad9b189d5ac6a01698281
Junming Zhang <junming@umich.edu>
2020-04-18 14:08:49 -0400

    add evaluation

---
+++ completionPC/evaluate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+docker run -it --rm \
+  --gpus '"device='0'"' \
+  -u $(id -u):$(id -g) \
+  -v $(pwd):/completionPC/cpc \
+  -v $(pwd)/../utils:/completionPC/utils \
+  -v $(pwd)/../data_root:/completionPC/data_root \
+  -w /completionPC/cpc \
+  completion-pc \
+  python main.py \
+  --eval \
+  --checkpoint checkpoint/cpc_b8e600s250lr2e-4_r025tr64-16_te16-16_occlu \
+  --categories Chair \
+  --num_pts 2048 \
+  --num_pts_observed 1024 \
+  --radius 0.25 \
+  --bottleneck 512 \
+  --num_subpc_train 64 \
+  --num_contri_feats_train 16 \
+  --num_subpc_test 16 \
+  --num_contri_feats_test 16
+  # --randRotY
--- completionPC/main.py
+++ completionPC/main.py
@@ -8,7 +8,7 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from utils.logger import Logger
 from utils.models import Model
-from utils.model_utils import SimuOcclusion, get_lr, chamfer_loss
+from utils.model_utils import SimuOcclusion, Bottom, get_lr, chamfer_loss
 from shutil import copyfile
 from torch.optim.lr_scheduler import StepLR
 from torchvision import transforms
@@ -25,7 +25,7 @@ np.random.seed(seed)
 torch.manual_seed(seed)
 
 
-def train_one_epoch(loader, epoch, check_dir):
+def train_one_epoch(loader, optimizer, logger, epoch):
 
     model.train()
     total_loss = []
@@ -55,7 +55,8 @@ def train_one_epoch(loader, epoch, check_dir):
         i = i + 1
 
 
-def test(loader, epoch, check_dir):
+def test_one_epoch(loader, logger, epoch, save_dir):
+
     model.eval()
     total_chamfer_loss = []
     trans = SimuOcclusion()
@@ -79,15 +80,82 @@ def test(loader, epoch, check_dir):
             generated_pc = generated_pc.cpu().detach().numpy()[0]
             generated_latent_pc = generated_latent_pc.cpu().detach().numpy()
 
-            np.save(os.path.join(check_dir, 'pos_{}'.format(epoch)), pos)
-            np.save(os.path.join(check_dir, 'pos_observed_{}'.format(epoch)), pos_observed)
-            np.save(os.path.join(check_dir, 'contribution_pc_{}'.format(epoch)), contribution_pc)
-            np.save(os.path.join(check_dir, 'generated_pc_{}'.format(epoch)), generated_pc)
-            np.save(os.path.join(check_dir, 'generated_latent_pc_{}'.format(epoch)), generated_latent_pc)
+            np.save(os.path.join(save_dir, 'pos_{}'.format(epoch)), pos)
+            np.save(os.path.join(save_dir, 'pos_observed_{}'.format(epoch)), pos_observed)
+            np.save(os.path.join(save_dir, 'contribution_pc_{}'.format(epoch)), contribution_pc)
+            np.save(os.path.join(save_dir, 'generated_pc_{}'.format(epoch)), generated_pc)
+            np.save(os.path.join(save_dir, 'generated_latent_pc_{}'.format(epoch)), generated_latent_pc)
 
     avg_chamfer_loss = sum(total_chamfer_loss) / len(total_chamfer_loss)
     logger.scalar_summary('test_chamfer_dist', avg_chamfer_loss, epoch)
     print('Epoch: {:03d}, eval_loss: {:.5f}\n'.format(epoch, avg_chamfer_loss))
+
+
+def train(args):
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
+    scheduler = StepLR(optimizer, step_size=args.step_size, gamma=0.2)
+
+    model_name = args.model_name
+    check_dir = 'checkpoint/{}'.format(model_name)
+    if not os.path.exists('checkpoint/{}'.format(model_name)):
+        os.makedirs('checkpoint/{}'.format(model_name))
+    save_dir = os.path.join(check_dir, 'train_sample_results')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    log_dir = './logs/{}'.format(model_name)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    logger = Logger(log_dir)
+    backup(log_dir, parser)
+
+    global i
+    i = 1
+    for epoch in range(1, args.max_epoch+1):
+        # do training
+        train_one_epoch(train_dataloader, optimizer, logger, epoch)
+        # reduce learning rate
+        scheduler.step()
+        # validation
+        test_one_epoch(test_dataloader, logger, epoch, save_dir)
+    # save model
+    torch.save(model.state_dict(), 'checkpoint/{}/model.pth'.format(model_name))
+
+
+def evaluate(args, save_dir):
+
+    model.eval()
+    total_chamfer_loss = []
+    trans = SimuOcclusion()
+
+    for j, data in enumerate(test_dataloader, 0):
+        data = data.to(device)
+        pos, batch = data.pos, data.batch
+        pos_observed, batch_observed = trans(pos, batch, args.num_pts_observed)
+
+        with torch.no_grad():
+            generated_pc, _ = model(None, pos_observed, batch_observed)
+            generated_latent_pc = model.generate_pc_from_latent(model.contribution_mean[0, 0, :].view(1, -1))
+            contribution_pc = model.contribution_pc
+        total_chamfer_loss.append(chamfer_loss(generated_pc, pos.view(-1, args.num_pts, 3)))
+
+        # save the first sample results for visualization
+        pos = pos.cpu().detach().numpy().reshape(-1, args.num_pts, 3)[0]
+        pos_observed = pos_observed.cpu().detach().numpy().reshape(-1, args.num_pts_observed, 3)[0]
+        contribution_pc = contribution_pc.cpu().detach().numpy()
+        generated_pc = generated_pc.cpu().detach().numpy()[0]
+        generated_latent_pc = generated_latent_pc.cpu().detach().numpy()
+
+        np.save(os.path.join(save_dir, 'pos_{}'.format(j)), pos)
+        np.save(os.path.join(save_dir, 'pos_observed_{}'.format(j)), pos_observed)
+        np.save(os.path.join(save_dir, 'contribution_pc_{}'.format(j)), contribution_pc)
+        np.save(os.path.join(save_dir, 'generated_pc_{}'.format(j)), generated_pc)
+        np.save(os.path.join(save_dir, 'generated_latent_pc_{}'.format(j)), generated_latent_pc)
+
+    avg_chamfer_loss = sum(total_chamfer_loss) / len(total_chamfer_loss)
+    print('{} point clouds are evaluated.'.format(len(test_dataset)))
+    print('Avg_chamfer_loss: {:.5f}'.format(avg_chamfer_loss))
+    print('Sample results are saved to: {}'.format(save_dir))
 
 
 def backup(log_dir, parser):
@@ -125,6 +193,8 @@ if __name__ == '__main__':
     parser.add_argument("--num_contri_feats_test", type=int, help="the number of contribution features during test")
     parser.add_argument("--is_fidReg", action='store_true', help="flag for fidelity regularization during training")
     parser.add_argument("--randRotY", action='store_true', help="flag for random rotation along Y axis")
+    parser.add_argument("--eval", action='store_true', help="flag for doing evaluation")
+    parser.add_argument("--checkpoint", type=str, help="directory which contains pretrained model (.pth)")
 
     args = parser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -134,7 +204,7 @@ if __name__ == '__main__':
     if args.randRotY:
         transform = T.Compose([T.FixedPoints(args.num_pts), T.RandomRotate(180, axis=1)])
     else:
-        transform = T.FixedPoints(args.num_pts)
+        transform = T.Compose([T.FixedPoints(args.num_pts)])
 
     train_dataset = ShapeNet('../data_root/ShapeNet_normal', categories, split='trainval',
                              include_normals=False, pre_transform=pre_transform, transform=transform)
@@ -152,42 +222,21 @@ if __name__ == '__main__':
                   num_contri_feats_train=args.num_contri_feats_train,
                   num_contri_feats_test=args.num_contri_feats_test,
                   is_fidReg=args.is_fidReg)
-    # to do: using multiple GPUs
-    # if torch.cuda.device_count() > 1:
-    #     print("Let's use", torch.cuda.device_count(), "GPUs!")
-    #     model = torch.nn.DataParallel(model)
     model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
-    scheduler = StepLR(optimizer, step_size=args.step_size, gamma=0.2)
+    # evaluation
+    if args.eval:
+        model_path = os.path.join(args.checkpoint, 'model.pth')
+        if not os.path.isfile(model_path):
+            raise ValueError('Please provide a valid path for pretrained model!'.format(model_path))
+        model.load_state_dict(torch.load(model_path))
+        print('Load model successfully from: {}'.format(args.checkpoint))
 
-    model_name = args.model_name
-    check_dir = 'checkpoint/{}'.format(model_name)
-    if not os.path.exists('checkpoint/{}'.format(model_name)):
-        os.makedirs('checkpoint/{}'.format(model_name))
+        save_dir = os.path.join(args.checkpoint, 'eval_sample_results')
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        evaluate(args=args, save_dir=save_dir)
 
-    log_dir = './logs/{}'.format(model_name)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    logger = Logger(log_dir)
-    # backup
-    backup(log_dir, parser)
-
-    global i
-    i = 1
-
-    # with GuruMeditation():
-    # with torch.autograd.detect_anomaly():
-    for epoch in range(1, args.max_epoch+1):
-
-        # do training
-        train_one_epoch(train_dataloader, epoch, check_dir)
-
-        # reduce learning rate
-        scheduler.step()
-
-        # evaluation
-        test(test_dataloader, epoch, check_dir)
-
-    # save model
-    torch.save(model.state_dict(), 'checkpoint/{}/model.pth'.format(model_name))
+    # training
+    else:
+        train(args=args)
