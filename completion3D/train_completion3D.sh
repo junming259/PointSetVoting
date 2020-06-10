ab607208c0dc0a497fb0064b9774f20bd5100802
Junming Zhang <junming@umich.edu>
2020-06-10 13:38:08 -0400

    Train and evaluate on Completion3D
    Training flags are not used in the evaluation, so delete them in
    evaluate.sh. Pow(2) in chamfer distance turns out to consume more GPU
    memeory.

---
+++ completion3D/train_completion3D.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+docker run -it --rm \
+  --gpus '"device='4,5,6,7'"' \
+  -u $(id -u):$(id -g) \
+  -v $(pwd):/cpc/training \
+  -v $(pwd)/../utils:/cpc/utils \
+  -v $(pwd)/../data_root:/cpc/data_root \
+  -w /cpc/training \
+  completion-pc \
+  python ../utils/main.py \
+  --model_name cpc3d_b64e500s200lr2e-4_r010tr64-10_te32_bn1024_trainval \
+  --dataset completion3D \
+  --categories plane,cabinet,car,chair,lamp,couch,table,watercraft \
+  --task completion \
+  --num_pts 2048 \
+  --num_pts_observed 2048 \
+  --lr 0.0002 \
+  --step_size 200 \
+  --max_epoch 500 \
+  --bsize 64 \
+  --radius 0.10 \
+  --bottleneck 1024 \
+  --num_vote_train 64 \
+  --num_contrib_vote_train 10 \
+  --num_vote_test 32 \
+  --is_vote \
+  --is_rand \
