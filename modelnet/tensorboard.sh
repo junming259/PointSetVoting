9edc68e766ac7691203712070da6880fe6203551
junming zhang <junming@umich.edu>
2020-12-30 00:47:24 -0500

    cls on modelnet
    Remove docker environment.

--- modelnet/tensorboard.sh
+++ modelnet/tensorboard.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-docker run -it --rm \
-  -u $(id -u):$(id -g) \
-  -v $PWD/logs:/logs:ro \
-  -p 6005:6006 \
-  completion-pc \
-  tensorboard --logdir=/logs --port=6006 --bind_all
+tensorboard --logdir=./logs --port=6006 --bind_all
--- modelnet/train_modelnet.sh
+++ modelnet/train_modelnet.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-docker run -it --rm \
-  --gpus '"device='6,7'"' \
-  -u $(id -u):$(id -g) \
-  -v $(pwd):/cpc/modelnet \
-  -v $(pwd)/../utils:/cpc/utils \
-  -v $(pwd)/../data_root:/cpc/data_root \
-  -w /cpc/modelnet \
-  completion-pc \
-  python ../utils/main.py \
-  --model_name test_cls_modelnet_b64e500s200lr1e-3_r020tr64-10_te16_bn1024_sphere_test \
-  --dataset modelnet \
-  --task classification \
-  --num_pts 1024 \
-  --num_pts_observed 512 \
-  --lr 0.001 \
-  --step_size 200 \
-  --max_epoch 500 \
-  --bsize 64 \
-  --radius 0.20 \
-  --bottleneck 1024 \
-  --num_vote_train 64 \
-  --num_contrib_vote_train 10 \
-  --num_vote_test 32 \
-  --is_vote \
-  --is_simuOcc \
-  --norm sphere \
+python ../utils/main.py \
+--model_name cls_b64e500s200lr1e-3_r020tr64-10_te32_bn1024 \
+--task classification \
+--num_pts 1024 \
+--num_pts_observed 512 \
+--lr 0.001 \
+--step_size 200 \
+--max_epoch 500 \
+--bsize 64 \
+--radius 0.20 \
+--bottleneck 1024 \
+--num_vote_train 64 \
+--num_contrib_vote_train 10 \
+--num_vote_test 32 \
+--is_vote \
+--is_simuOcc \
+--norm scale \
+
